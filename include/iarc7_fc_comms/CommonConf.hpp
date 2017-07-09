41430860d90b576042c0b1334a2941a3b5a68f11
Jetson TX1 <ras@pitt.edu>
2017-07-08 21:03:36 -0400

    Adjust accelerometer variance

--- include/iarc7_fc_comms/CommonConf.hpp
+++ include/iarc7_fc_comms/CommonConf.hpp
@@ -45,7 +45,7 @@ struct CommonConf
     static constexpr const char* kTfChildName{"quad"};
 
     // Variance of acceleration measurements (in m/2^2)
-    static constexpr const double kAccelerationVariance[3] = {0.09, 0.09, 0.09};
+    static constexpr const double kAccelerationVariance[3] = {0.04, 0.04, 0.04};
 };
 
 } // namespace FcComms
