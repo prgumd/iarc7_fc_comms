3c3e50b1558b9ce0786485a83311fb20301c49d9
Levi Burner <leviburner@gmail.com>
2019-09-20 20:05:44 -0400

    Make motor script test arming over and over

--- scripts/motors_test.py
+++ scripts/motors_test.py
@@ -4,8 +4,6 @@ import tf
 
 import math
 
-from iarc7_safety.SafetyClient import SafetyClient
-
 from iarc7_msgs.msg import OrientationThrottleStamped
 from iarc7_msgs.msg import BoolStamped
 from iarc7_msgs.srv import Arm
@@ -18,56 +16,48 @@ if __name__ == '__main__':
     rospy.wait_for_service('uav_arm')
     arm_service = rospy.ServiceProxy('uav_arm', Arm)
     rospy.logerr('done waiting')
-    #safety_client = SafetyClient('motors_test')
-    #safety_client.form_bond()
 
     command_pub = rospy.Publisher('uav_direction_command', OrientationThrottleStamped, queue_size=0)
 
-    armed = False
-    while armed == False :
-        try:
-            rospy.logerr('Trying to arm')
-            armed = arm_service(True)
-        except rospy.ServiceException as exc:
-            rospy.logerr('Could not arm: ' + str(exc))
-    rospy.logerr('Armed')
     rate = rospy.Rate(30)
     throttle = 0
     while not rospy.is_shutdown():
+        armed = False
+        while armed == False :
+            try:
+                rospy.logerr('Trying to arm')
+                armed = arm_service(True)
+            except rospy.ServiceException as exc:
+                rospy.logerr('Could not arm: ' + str(exc))
+        rospy.logerr('Armed')
 
-        # Exit immediately if fatal
-        # if safety_client.is_fatal_active():
-        #     break;
-
-        # Turn off motors if put into safety mode
-        # if safety_client.is_safety_active():
-        #     try:
-        #         arm_service(False)
-        #     except rospy.ServiceException as exc:
-        #         rospy.loggerr("Could not disarm: " + str(exc))
-
-        #     command = OrientationThrottleStamped()
-        #     command.header.stamp = rospy.Time.now()
-        #     command_pub.publish(command)
-
-        # else:
         command = OrientationThrottleStamped()
         command.header.stamp = rospy.Time.now()
 
-        # Three second throttle ramp and three seconds off
-        throttle = (throttle + 1) % 150
-        if throttle <= 100:
-            command.throttle = float(throttle)/100.0
-            command.data.pitch = 0.0
-            command.data.roll = 0.0
-            command.data.yaw = 0.0
-            command_pub.publish(command)
-        else:
-            command.throttle = 0.0
-            command.data.pitch = 0.0
-            command.data.roll = 0.0
-            command.data.yaw = 0.0
-            command_pub.publish(command)
+        while not rospy.is_shutdown():
+            # Three second throttle ramp and three seconds off
+            throttle = (throttle + 1) % 150
+            if throttle <= 100:
+                command.throttle = float(throttle)/100.0
+                command.data.pitch = 0.0
+                command.data.roll = 0.0
+                command.data.yaw = 0.0
+                command_pub.publish(command)
+            else:
+                command.throttle = 0.0
+                command.data.pitch = 0.0
+                command.data.roll = 0.0
+                command.data.yaw = 0.0
+                command_pub.publish(command)
 
-        rate.sleep()
+            rate.sleep()
 
+        while armed == True :
+            try:
+                rospy.logerr('Trying to disarm')
+                armed = arm_service(False)
+            except rospy.ServiceException as exc:
+                rospy.logerr('Could not disarm: ' + str(exc))
+        rospy.logerr('disarmed')
+
+        rospy.Time.sleep(2.0)
